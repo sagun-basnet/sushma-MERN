dab507eae924f27b9afa17e12c4d7395b80084ee
sagun basnet <basnetsagun8@gmail.com>
2025-12-11 12:24:56 +0545

    todo list

--- react/src/pages/About.jsx
+++ react/src/pages/About.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import Card from "../components/Card";
 
 const About = () => {
+  const data = [
+    {
+      name: "Jhon",
+      age: 34,
+      email: "jhon@gmail.com",
+    },
+    {
+      name: "Jhon",
+      age: 34,
+      email: "jhon@gmail.com",
+    },
+    {
+      name: "Jhon",
+      age: 34,
+      email: "jhon@gmail.com",
+    },
+    {
+      name: "Jhon",
+      age: 34,
+      email: "jhon@gmail.com",
+    },
+  ];
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <div className="grid grid-cols-3">
+        {data.map((item, index) => {
+          return (
+            <Card
+              name={item.name}
+              age={item.age}
+              email={item.email}
+              index={index}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
-export default About
+export default About;
